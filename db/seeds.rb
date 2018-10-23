daccee9a1bb9e3d2173fcdd6f1e63a7ab1e8ff5b
aaron <aaroncoleg@yahoo.com>
2018-10-23 14:42:48 -0500

    working on login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.create([{ name: 'User', level:  1},
+                   { name: 'Sales person', level: 2 },
+                   { name: 'Sales manager', level: 3 },
+                   { name: 'Finance manager', level:  4},
+                   { name: 'Inventory manager', level:  5},
+                   { name: 'Dealership owner', level:  6}
+                  ])
