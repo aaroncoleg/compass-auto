7ea9a9066b2d63965cd888ff0192d5e83aebf942
Light <estebanaguirre47@gmail.com>
2018-11-02 17:28:10 -0600

    Merge remote-tracking branch 'origin/dev' into dev
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -55,5 +55,30 @@ ActiveRecord::Schema.define(version: 2018_11_02_214512) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
   end
+  create_table "colors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
+  create_table "inventories", force: :cascade do |t|
+    t.integer "miles"
+    t.integer "year"
+    t.string "model"
+    t.integer "vin"
+    t.boolean "sold"
+    t.decimal "price"
+    t.integer "color_id"
+    t.integer "make_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["color_id"], name: "index_inventories_on_color_id"
+    t.index ["make_id"], name: "index_inventories_on_make_id"
+  end
+
+  create_table "makes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
