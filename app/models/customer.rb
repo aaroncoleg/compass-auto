6520da5f307ba5e1f4f8e17c044efea98a129ab5
Light <estebanaguirre47@gmail.com>
2018-11-20 12:42:52 -0600

    Esteban Aguirre pushing changes to the application.html in the view file and the quote_show_page. Just adding links for financial breakdown and nav bar

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
   belongs_to :state
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,4 @@
 class Quote < ApplicationRecord
   belongs_to :customer
   belongs_to :inventory
-
-end
+  end
