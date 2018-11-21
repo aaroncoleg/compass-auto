ec3438624a345b5e2a95942b7faf82ec8cba6908
aaron <aaroncoleg@yahoo.com>
2018-11-20 18:35:15 -0600

    fixed schema and migrations

--- db/migrate/20181115025530_create_models.rb
+++ db/migrate/20181115025530_create_models.rb
@@ -2,7 +2,7 @@ class CreateModels < ActiveRecord::Migration[5.2]
   def change
     create_table :models do |t|
       t.references :make, foreign_key: true
-      t.string :model_name
+      t.string :name
 
       t.timestamps
     end
